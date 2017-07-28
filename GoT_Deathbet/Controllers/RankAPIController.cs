e0897b47b7daf8f172d61a2894939b79d4c42029
Ben Mizera <mizerb@rpi.edu>
2017-07-28 14:49:58 -0400

    Ranking API setup
    Page and API setup for the rankings page of the app.

---
+++ GoT_Deathbet/Controllers/RankAPIController.cs
@@ -0,0 +1,25 @@
+﻿using GoT_Deathbet.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+namespace GoT_Deathbet.Controllers
+{
+    public class RankAPIController : ApiController
+    {
+        const string database_name = @"C:\CODE\git\GoT_Deathbet\GoT_Deathbet\App_Data\db.sqlite";
+
+        public JsonResult<Candidate[]> GetRanks()
+        {
+            using (SQLite.SQLiteConnection db = new SQLite.SQLiteConnection(database_name))
+            {
+                return Json(db.Table<Candidate>().OrderByDescending(x => x.elo).Take(20).ToArray());
+            }
+        }
+
+    }
+}
---
+++ GoT_Deathbet/Controllers/RankingController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GoT_Deathbet.Controllers
+{
+    public class RankingController : Controller
+    {
+        // GET: Ranking
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
